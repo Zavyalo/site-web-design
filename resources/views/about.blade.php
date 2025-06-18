5f2c2259b9816e6ff7a26525e452b47793999f68
Sophie <bersenjowa@gmail.com>
2025-06-19 00:31:00 +0500

    sonia:about upd

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,77 @@
 <x-app-layout>
-    О нас
+
+<section class="about-desc">
+    <div class="about-desc-text-wrapper grid grid-cols-2">
+        <h1 class="text-4xl">О НАС</h1>
+        <p>«Цифровые решения» — это мастерская МИДиСа, одного из ведущих образовательных учреждений в области дизайна, сервиса и цифровых технологий.</p>
+        <p class="col-start-2">Наша команда объединяет студентов, преподавателей и специалистов, стремящихся создавать современные цифровые продукты: от исследований и прототипов до сайтов и VR-приложений.</p>
+    </div>
+</section>
+
+<section class="about-mission">
+    <h1 class="text-4xl">МИССИЯ И ЦЕННОСТИ</h1>
+    <div class="about-blob-wrapper grid grid-cols-3 gap-[14px]">
+        <!-- row 1 -->
+        <div class="bg-white flex flex-1 rounded-[100px] p-8 items-center justify-center">Упростить сложное</div>
+        <div class="bg-gradient flex flex-1 clip-sloped-25 p-8 items-center justify-center"></div>
+        <div class="bg-white flex flex-1 rounded-xl p-8 items-center justify-center">Всегда работать в интересах клиента</div>
+        <!-- row 2 -->
+        <div class="bg-gradient flex flex-1 rounded-[100px] p-8 justify-center"></div>
+        <div class="bg-white flex flex-1 rounded-xl p-8 justify-center">Быть открытыми к экспериментам и технологиям</div>
+        <div class="bg-gradient flex flex-1 rounded-[100px] p-8 justify-center"></div>
+        <!-- row 3 -->
+        <div class="bg-white flex flex-1 rounded-[100px] p-8 justify-center"></div>
+        <div class="bg-gradient flex flex-1 clip-sloped-25 p-8 justify-center"></div>
+        <div class="bg-white flex flex-1 rounded-xl p-8 justify-center">Создавать решения, а не просто дизайн</div>
+    </div>
+</section>
+
+<section class="about-goals">
+    <h1 class="text-4xl">ДОСТИЖЕНИЯ</h1>
+    <div class="about-goals-num-wrapper flex flex-row">
+        <div class="about-goals-num flex flex-1 flex-col grow items-center">
+            <h2 class="text-[200px] font-bold">80+</h2>
+            <span>Завершённых проектов</span>
+        </div>
+        <div class="about-goals-num flex flex-1 flex-col grow items-center">
+            <h2 class="text-[200px] font-bold">4</h2>
+            <span>Направления работ</span>
+        </div>
+        <div class="about-goals-num flex flex-1 flex-col grow items-center">
+            <h2 class="text-[200px] font-bold">25</h2>
+            <span>Постоянных клиентов</span>
+        </div>
+    </div>
+</section>
+
+<section class="about-experts">
+    <div class="about-experts-text-wrapper grid grid-cols-2">
+        <h1 class="text-4xl">НАШИ СПЕЦИАЛИСТЫ</h1>
+        <p>Каждый проект — это синергия UX-аналитиков, дизайнеров, контент-специалистов, верстальщиков и VR-разработчиков. В команде — специалисты с опытом от 3 до 12 лет</p>
+    </div>
+    <div class="about-experts-photo-wrapper flex flex-row gap-[14px]">
+        <div class="bg-white flex flex-1 rounded-xl p-8 items-center justify-center">1</div>
+        <div class="bg-white flex flex-1 rounded-[100px] p-8 justify-center">2</div>
+        <div class="bg-white flex flex-1 clip-sloped-25 p-8 justify-center">3</div>
+        <div class="bg-white flex flex-1 rounded-xl p-8 items-center justify-center">4</div>
+        <div class="bg-white flex flex-1 rounded-[100px] p-8 justify-center">5</div>
+    </div>
+</section>
+
+<section class="about-awards">
+    <div class="about-awards-text-wrapper flex flex-row">
+        <div class="about-awards-text flex flex-col p-[42px] h-[400px] justify-between grow items-start">
+            <h2 class="text-5xl font-medium">Digital Design Award 2024</h2>
+            <span>Участники и финалисты </span>
+        </div>
+        <div class="about-awards-text flex flex-col p-[42px] h-[400px] justify-between grow items-start">
+            <h2 class="text-5xl font-medium">Tilda Education и VR Days</h2>
+            <span>Партнёры конференций</span>
+        </div>
+        <div class="about-awards-text flex flex-col p-[42px] h-[400px] justify-between grow items-start">
+            <h2 class="text-5xl font-medium">Nielsen  Norman Group</h2>
+            <span>Сертификаты по UX </span>
+        </div>
+    </div>
+</section>
 </x-app-layout>
