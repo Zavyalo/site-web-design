49bce9a1a89ce394b4c3e20164ec3c65b0f29a21
alenashmelts <alenashmelts@yandex.ru>
2025-06-19 12:54:10 +0500

    create welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,48 @@
 <x-app-layout>
-    <p class="text-3xl font-bold text-blue-600">Главная страница</p>
-    <x-secondary-button>Кнопка</x-secondary-button>
-    <ul class="blog-ul">
-        <li>элемент списка</li>
-        <li>элемент списка</li>
-    </ul>
+    <div flex flex-col size-auto gap-[144px]>
+        <div class="">
+        <p class="text-3xl font-bold text-white-600">Создаём цифровые решения, которые работают</p>
+        <h1 class="">Образовательный проект МИДиС: от идеи до цифрового продукта. Мы слышим, вникаем и превращаем Ваши задачи в понятные, живые цифровые продукты</h1>
+        <x-secondary-button>Оставить заявку</x-secondary-button>
+        </div>
+        <div class="flex flex-col size-auto gap-[48px]">
+            <h1>наши услуги</h1>
+            <div class="flex flex-wrap gap-[14px]">
+                <div class="border border-white outline-[1,5px] rounded-[28px] w-[866px] h-[413px]">
+                    <h1>исследования и гипотезы</h1>
+                </div>
+                <div class="border border-white outline-[1,5px] rounded-[28px] w-[426px] h-[413px]">
+                    <h1>Создание сайтов на tilda</h1>
+                </div>
+                <div class="border border-white outline-[1,5px] rounded-[28px] w-[866px] h-[413px]">
+                    <h1>Создание приложений с vr-технологиями</h1>
+                </div>
+                <div class="border border-white outline-[1,5px] rounded-[28px] w-[426px] h-[413px]">
+                    <h1>Создание прототипов сайта</h1>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col size-auto gap-[48px]">
+            <h1>почему выбирают нас</h1>
+            <div class="flex flex-row gap-[14px]">
+                <div class="flex justify-center items-center border border-white outline-[1,5px] rounded-[28px] w-[426px] h-[365px]">
+                    <h1>Тестирование вашего сайта за 1 день с участием >500 человек</h1>
+                </div>
+                <div class="flex justify-center items-center border border-white outline-[1,5px] rounded-[170px] w-[426px] h-[365px]">
+                    <h1>эффективное решение ваших задач благодаря работе большой команды</h1>
+                </div>
+                <div class="flex justify-center items-center border border-white outline-[1,5px] rounded-[28px] w-[426px] h-[365px]">
+                    <h1>Прозрачные сроки и процессы</h1>
+                </div>
+                <div class="flex justify-center items-center border border-white outline-[1,5px] rounded-[170px] w-[426px] h-[365px]">
+                    <h1>Опыт в разных отраслях: EdTech, ритейл, медицина, культура</h1>
+                </div>
+                <div class="flex justify-center items-center border border-white outline-[1,5px] rounded-[28px] w-[426px] h-[365px]">
+                    <h1>Глубокая 
+                        UX-экспертиза</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </x-app-layout>
