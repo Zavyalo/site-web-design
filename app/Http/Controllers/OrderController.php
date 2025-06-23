bdcdfec9c21800c4a0ff8ff22b7073da980a55cf
JuliaMuk <Lector@rbiu.local>
2025-06-23 11:48:53 +0500

    add controller for order

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function store (Request $request, Order $order)
+    {
+        $data = $request -> validate([
+            'name'=>'required|string|max:255',
+            'tel'=>'required|string|max:255',
+            'email'=>'required|string|max:255',
+            'comment'=>'string'
+        ]);
+        $order->create($data);
+        return redirect()->back()->with('info','Ваше сообщение принято! Мы свяжемся с Вами в ближайшее время!');
+    }
+}
