83891d7072a3c90bb310e78db95072fe83baffc9
kate356-ff <elenamanaeva18@gmail.com>
2025-06-24 12:46:06 +0500

    portfolio page

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -11,7 +11,8 @@ class WorkController extends Controller
     public function index()
     {
         $works = Work::all();
-        return view('portfolio',compact('works'));
+        $tags = Tag::all();
+        return view('portfolio',compact('works','tags'));
     }
     
     public function show(Work $work)
