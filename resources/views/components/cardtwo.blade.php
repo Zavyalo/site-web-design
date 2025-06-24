83d78e1581fc9d65f727dc4b6b4db03989656883
Ty4ka312 <pazgeorg@gmail.com>
2025-06-24 12:07:33 +0500

    cardtwo

--- resources/views/components/cardtwo.blade.php
+++ resources/views/components/cardtwo.blade.php
@@ -1,21 +1,21 @@
 <div class="flex justify-center items-center h-screen bg-gray-900">
   <div class="flex justify-center items-center border border-white outline-[1,5px] rounded-[28px] w-[774px] h-[392px]">
-      <div class="flex flex-row gap-[80px] py-14 px-14">
-          <div class="space-y-1 justify-center size-auto flex flex-wrap gap-[15px]">
-              <img src="{{ Vite::asset('resources/images/Mask group.jpg') }}" alt="Влад Зелинский" class="w-[180px] rounded-full">
-            <div>
-              <h1 class="text-base/[20px] font-semibold w-[178px] h-[14px]">Влад Зелинский</h1>
-            </div>
-            <div>
-              <h1 class="space-y-0 text-sm/[18px] font-medium w-[174px] h-[28px]">Head of design at Miro, Выпускник 2015 года</h1>
-            </div>
-          </div>
-          <div class="flex size-auto flex-wrap">
-              <blockquote class="mt-4 w-[427px] h-[217px]">
-                  <h1 class="font-bold text-7xl">"</h1>
-                  <h1 class="font-regular text-base/[20px] w-[427px] h-[168px]">Для меня дизайн - это не про работу, а про подход к решению жизненных задач. В Miro мы каждый день помогаем миллионам людей решать их задачи. Лидерство в дизайне привносит в этот процесс красоту и ясность. Я рад быть частью этого движения</h1>
-              </blockquote>
-          </div>
+    <div class="flex flex-row gap-[60px] m-10">
+      <div class="flex flex-col items-center gap-[15px] size-auto">
+        <img src="{{ Vite::asset('resources/images/Mask group.jpg') }}" alt="Влад Зелинский" class="w-[180px] rounded-full">   
+        <h1 class="tracking-wider font-normal text-white text-lg/[20px] font-semibold text-center w-[178px] h-[14px]">Влад Зелинский</h1>
+        <h1 class="tracking-wider font-light text-white text-sm/[18px] font-medium text-center w-[174px] h-[28px]">Head of design at Miro, Выпускник 2015 года</h1>
       </div>
+      <div class="flex size-auto flex-wrap">
+        <blockquote class="mt-4 w-[427px] h-[217px]">
+          <h1 class="text-white font-bold text-7xl">"</h1>
+          <h1 class="tracking-wider font-light text-white font-regular text-lg/[25px] w-[427px] h-[168px]">
+            Для меня дизайн - это не про работу, а про подход к решению жизненных задач. В Miro мы каждый день 
+            помогаем миллионам людей решать их задачи. Лидерство в дизайне привносит в этот процесс красоту и ясность. 
+            Я рад быть частью этого движения
+          </h1>
+        </blockquote>
+      </div>
+    </div>
   </div>
 </div>
