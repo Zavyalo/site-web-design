4bb3f9a387f586dbf786290c1733d15c21d57eab
zavyalo <pngzavyalova@yandex.ru>
2025-06-26 10:42:54 +0500

    FQA

--- resources/views/components/FAQ.blade.php
+++ resources/views/components/FAQ.blade.php
@@ -1,9 +1,37 @@
 <section class="flex flex-row justify-between w-full font-inter">
     <x-h1-white>ВОПРОСЫ <br> И ОТВЕТЫ</x-h1-white>
-    <div class="flex flex-col w-200">
-        <div class="flex flex-row justify-between text-white text-lg form__border py-space-32">
+    <div class="flex flex-col w-[1000px] gap-space-26">
+        <div class="text-white text-lg form__border pb-space-32">
+        <div class="flex flex-row justify-between">
             <p class="">Как мы передаём сайт клиенту?</p>
             <div class="" id="">⮧</div>
+        </div>
+        <p class="pt-space-32 hidden">Мы переносим готовый сайт в ваш аккаунт на Tilda, а также предоставляем инструкции по управлению сайтом.</p>
+        </div>
+        <div class="text-white text-lg form__border pb-space-32">
+            <div class="flex flex-row justify-between">
+                <p class="">Можно ли потом редактировать сайт самому?</p>
+                <div class="" id="">⮧</div>
+            </div>
+            <p class="pt-space-32 hidden">Да, вы сможете самостоятельно редактировать сайт через редактор Tilda. Для этого нужно войти, выбрать нужный проект и перейти в режим редактирования. Вы сможете менять тексты, изображения, добавлять новые блоки, корректировать дизайн и структуру страниц. Мы предоставим инструкции и, при необходимости, проведём обучение.</p>
+        </div>
+        <div class="text-white text-lg form__border pb-space-32">
+            <div class="flex flex-row justify-between">
+                <p class="">Какие этапы включает процесс разработки цифрового продукта?</p>
+                <div class="" id="">⮧</div>
+            </div>
+            <ul class="pt-space-32 hidden"> 
+                <li>Исследование и аналитика</li>
+                <li>Проектирование и дизайн</li>
+                <li>Верстка на Tilda</li>
+            </ul>
+        </div>
+        <div class="text-white text-lg form__border pb-space-32">
+            <div class="flex flex-row justify-between">
+                <p class="">Предоставляете ли вы услуги по аудиту сайта?</p>
+                <div class="" id="">⮧</div>
+            </div>
+            <p class="pt-space-32 hidden">Да, мы проводим аудит сайтов. В рамках аудита мы анализируем дизайн, структуру, юзабилити, технические параметры и показатели эффективности сайта. По результатам мы предоставляем подробный отчёт с рекомендациями по улучшению.</p>
         </div>
     </div>
 </section>
