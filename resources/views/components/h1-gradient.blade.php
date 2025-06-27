e2ec6bdac290868f456497777828631c5b721131
zavyalo <pngzavyalova@yandex.ru>
2025-06-27 11:52:31 +0500

    h1 white

--- resources/views/components/h1-gradient.blade.php
+++ resources/views/components/h1-gradient.blade.php
@@ -1,3 +1,3 @@
-<h1 {{ $attributes->merge(['class' => 'font-victor bg-gradient-text-transparent bg-clip-text bg-gradient-to-r from-emerald-100 from-10% via-orange-500 via-20% to-red-200 to-100% bg-clip-text uppercase text-transparent text-6xl font-bold max-md:text-5xl max-[425px]:text-3xl']) }}>
+<h1 {{ $attributes->merge(['class' => 'font-victor bg-gradient-text-transparent bg-clip-text bg-gradient-to-r from-emerald-100 from-10% via-orange-500 via-20% to-red-200 to-100% bg-clip-text uppercase text-transparent text-6xl font-bold mb-space-144 max-md:text-5xl max-[425px]:text-3xl']) }}>
     {{ $slot }}
 </h1>
