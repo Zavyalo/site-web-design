0ce2821ab87efdf06f23e145bf68ab1e26134efe
alenashmelts <alenashmelts@yandex.ru>
2025-06-27 11:57:50 +0500

    swiper welcome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+import Swiper from 'swiper/bundle';
+
 import './bootstrap';
 
 import './burger';
@@ -12,3 +14,93 @@ Alpine.start();
 import.meta.glob([
         '../images/**',
 ]);
+
+window.onload = function(){
+    if(document.querySelector('.SwiperWelcome')){
+        const swiper = new Swiper('.SwiperWelcome', {
+            spaceBetween: 15,
+            slidesPerView: 4,
+            breakpoints: {
+                // when window width is >= 640px
+                320: {
+                  slidesPerView: 2,
+                  spaceBetween: 6
+                },
+                640: {
+                    slidesPerView: 3,
+                    spaceBetween: 6
+                },
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 6
+                },
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 15
+                },
+                1280: {
+                    slidesPerView: 3,
+                    spaceBetween: 15
+                },
+                1440: {
+                    slidesPerView: 3,
+                    spaceBetween: 15
+                },
+                1536: {
+                    slidesPerView: 3,
+                    spaceBetween: 15
+                },
+            speed: 1000,
+            loop: true,
+            autoplay: {
+              delay: 1500,
+              disableOnInteraction: false,
+            }
+            }
+        })
+    }
+
+    if(document.querySelector('.SwiperWelcomepartners')){
+        const swiper = new Swiper('.SwiperWelcomepartners', {
+            spaceBetween: 15,
+            slidesPerView: 5,
+            breakpoints: {
+                // when window width is >= 640px
+                320: {
+                  slidesPerView: 2,
+                  spaceBetween: 6
+                },
+                640: {
+                    slidesPerView: 3,
+                    spaceBetween: 6
+                },
+                768: {
+                    slidesPerView: 4,
+                    spaceBetween: 6
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 15
+                },
+                1280: {
+                    slidesPerView: 5,
+                    spaceBetween: 15
+                },
+                1440: {
+                    slidesPerView: 5,
+                    spaceBetween: 15
+                },
+                1536: {
+                    slidesPerView: 5,
+                    spaceBetween: 15
+                },
+            speed: 1000,
+            loop: true,
+            autoplay: {
+              delay: 1500,
+              disableOnInteraction: false,
+            }
+            }
+        })
+    }
+};
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,21 +12,25 @@
         <section class="flex flex-col gap-[48px] font-victor w-[100%]">
             <x-h1-gradient>НАШИ УСЛУГИ</x-h1-gradient>
             <div class="flex flex-wrap gap-[14px]">
-                <a href="/services#service1" class="flex flex-col justify-between grow bg-white clip-sloped-25 p-[35px] content-end 2xl:gap-[auto] xl:gap-[auto] lg:gap-[auto] md:gap-[auto] sm:gap-[0px] min-[320px]:gap-[55px]">
+                <a href="/services#service1" class="flex flex-col justify-between grow bg-white clip-sloped-25 p-[35px] content-end 2xl:gap-[auto] xl:gap-[auto] lg:gap-[auto] md:gap-[auto] sm:gap-[0px] min-[320px]:gap-[55px]
+                hover:bg-darkgray hover:text-white">
                     <div class="max-w-[100%] flex flex-row justify-end"><img src="{{ Vite::asset('resources/images/ArrowUpRight.svg') }}" alt="#" class="w-[50px] sm:w-[50px] min-[320px]:w-[30px]"></div>
-                    <h1 class="text-[64px] 2xl:text-[64px] lg:text-[64px] sm:text-[48px] min-[320px]:text-[16px] 2xl:max-w-[100%] lg:max-w-[465px]">ИССЛЕДОВАНИЯ И ГИПОТЕЗЫ</h1>
+                    <p class="text-[64px] 2xl:text-[64px] lg:text-[64px] sm:text-[48px] min-[320px]:text-[16px] 2xl:max-w-[100%] lg:max-w-[465px]">ИССЛЕДОВАНИЯ И ГИПОТЕЗЫ</p>
                 </a>
-                <a href="/services#service2" class="flex flex-col justify-between grow border border-white text-white outline-[1,5px] rounded-[28px] min-[320px]:w-[320px] md:w-[426px] xl:gap-[128px] lg:gap-[72px] sm:gap-[0px] min-[320px]:gap-[55px] p-[35px] content-end shrink-0 ">
+                <a href="/services#service2" class="flex flex-col justify-between grow border border-white text-white outline-[1,5px] rounded-[28px] min-[320px]:w-[320px] md:w-[426px] xl:gap-[128px] lg:gap-[72px] sm:gap-[0px] min-[320px]:gap-[55px] p-[35px] content-end shrink-0
+                hover:bg-darkgray hover:text-white">
                     <div class="max-w-[100%] flex flex-row justify-end"><img src="{{ Vite::asset('resources/images/ArrowUpRightwhite.svg') }}" alt="#" class="w-[50px] sm:w-[50px] min-[320px]:w-[30px]"></div>
-                    <h1 class="text-[48px] sm:text-[48px] min-[320px]:text-[16px]">СОЗДАНИЕ САЙТОВ НА TILDA</h1>
+                    <p class="text-[48px] sm:text-[48px] min-[320px]:text-[16px]">СОЗДАНИЕ САЙТОВ НА TILDA</p>
                 </a>
-                <a href="/services#service3" class="flex flex-col justify-between grow border border-white text-white outline-[1,5px] rounded-[28px] min-[320px]:w-[320px] md:w-[426px] gap-[63px] sm:gap-[0px] min-[320px]:gap-[55px] p-[35px] content-end shrink-0 ">
+                <a href="/services#service3" class="flex flex-col justify-between grow border border-white text-white outline-[1,5px] rounded-[28px] min-[320px]:w-[320px] md:w-[426px] gap-[63px] sm:gap-[0px] min-[320px]:gap-[55px] p-[35px] content-end shrink-0
+                hover:bg-darkgray hover:text-white">
                     <div class="max-w-[100%] flex flex-row justify-end"><img src="{{ Vite::asset('resources/images/ArrowUpRightwhite.svg') }}" alt="#" class="w-[50px] sm:w-[50px] min-[320px]:w-[30px]"></div>
-                    <h1 class="text-[48px] sm:text-[48px] min-[320px]:text-[16px]">СОЗДАНИЕ ПРИЛОЖЕНИЙ С VR-ТЕХНОЛОГИЯМИ</h1>
+                    <p class="text-[48px] sm:text-[48px] min-[320px]:text-[16px]">СОЗДАНИЕ ПРИЛОЖЕНИЙ С VR-ТЕХНОЛОГИЯМИ</p>
                 </a>
-                <a href="/services#service4" class="flex flex-col justify-between grow bg-white clip-sloped-25 p-[35px] content-end lg:gap-[auto] md:gap-[auto] sm:gap-[0px] min-[320px]:gap-[55px]">
+                <a href="/services#service4" class="flex flex-col justify-between grow bg-white clip-sloped-25 p-[35px] content-end lg:gap-[auto] md:gap-[auto] sm:gap-[0px] min-[320px]:gap-[55px]
+                hover:bg-darkgray hover:text-white">
                     <div class="max-w-[100%] flex flex-row justify-end"><img src="{{ Vite::asset('resources/images/ArrowUpRight.svg') }}" alt="#" class="w-[50px] sm:w-[50px] min-[320px]:w-[30px]"></div>
-                    <h1 class="text-[64px] 2xl:text-[64px] lg:text-[64px] sm:text-[48px] min-[320px]:text-[16px] 2xl:max-w-[100%]  xl:max-w-[689px] lg:max-w-[384px] ">СОЗДАНИЕ ПРОТОТИПОВ САЙТА</h1>
+                    <p class="text-[64px] 2xl:text-[64px] lg:text-[64px] sm:text-[48px] min-[320px]:text-[16px] 2xl:max-w-[100%]  xl:max-w-[689px] lg:max-w-[384px] ">СОЗДАНИЕ ПРОТОТИПОВ САЙТА</p>
                 </a>
             </div>
         </section>
@@ -54,51 +58,55 @@
 
         <section class="flex flex-col gap-[48px] font-victor w-[100%]">
             <x-h1-gradient class="text-[64px] text-white">ПОЧЕМУ ВЫБИРАЮТ НАС</x-h1-gradient>
-            <div class="flex flex-row gap-[14px] overflow-hidden">
-                <div class="flex justify-center items-center outline-[1,5px] shrink-0 rounded-[28px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-gradient p-[63px]">
+           <div class="swiper SwiperWelcome w-[100%]">
+            <div class="swiper-wrapper flex flex-row gap-[14px] w-[100%]">
+                <div class="swiper-slide flex justify-center items-center shrink-0 outline-[1,5px] rounded-[28px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-gradient p-[63px]">
                     <h1 class="text-[32px] sm:text-[32px] min-[320px]:text-[12px] text-white">ТЕСТИРОВАНИЕ ВАШЕГО САЙТА ЗА 1 ДЕНЬ С УЧАСТИЕМ >500 ЧЕЛОВЕК</h1>
                 </div>
-                <div class="flex justify-center items-center outline-[1,5px] shrink-0 rounded-[170px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-white p-[52px]">
+                <div class="swiper-slide flex justify-center items-center shrink-0 outline-[1,5px] rounded-[170px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-white p-[52px]">
                     <h1 class="text-[28px] sm:text-[28px] min-[320px]:text-[12px]">ЭФФЕКТИВНОЕ РЕШЕНИЕ ВАШИХ ЗАДАЧ БЛАГОДАРЯ РАБОТЕ БОЛЬШОЙ КОМАНДЫ</h1>
                 </div>
-                <div class="flex justify-center items-center outline-[1,5px] shrink-0 rounded-[28px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-gradient clip-sloped-25 px-[55px]">
+                <div class="swiper-slide flex justify-center items-center shrink-0 outline-[1,5px] rounded-[28px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-gradient clip-sloped-25 px-[55px]">
                     <h1 class="text-[32px] sm:text-[32px] min-[320px]:text-[12px] text-white">ПРОЗРАЧНЫЕ СРОКИ И ПРОЦЕССЫ</h1>
                 </div>
-                <div class="flex justify-center items-center outline-[1,5px] shrink-0 rounded-[170px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-white p-[52px]">
+                <div class="swiper-slide flex justify-center items-center shrink-0 outline-[1,5px] rounded-[170px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-white p-[52px]">
                     <h1 class="text-[28px] sm:text-[28px] min-[320px]:text-[12px]">ОПЫТ В РАЗНЫХ ОТРАСЛЯХ: EDTECH, РИТЕЙЛ, МЕДИЦИНА, КУЛЬТУРА</h1>
                 </div>
-                <div class="flex justify-center items-center outline-[1,5px] shrink-0 rounded-[28px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-gradient p-[63px]">
+                <div class="swiper-slide flex justify-center items-center shrink-0 outline-[1,5px] rounded-[28px] w-[426px] sm:w-[426px] min-[320px]:w-[184px] h-[365px]  sm:h-[365px] min-[320px]:h-[158px] bg-gradient p-[63px]">
                     <h1 class="text-[32px] sm:text-[32px] min-[320px]:text-[12px] text-white">ГЛУБОКАЯ UX-ЭКСПЕРТИЗА</h1>
                 </div>
             </div>
+        </div>
         </section>
         <section class="flex flex-col gap-[48px] w-[100%] font-inter">
             <x-h1-gradient class="text-[64px] text-white font-victor">НАШИ ПАРТНЕРЫ</x-h1-gradient>
-            <div class="flex flex-row grow gap-[8px] overflow-hidden">
-                <div class="flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
-                    <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-aero.png') }}" alt="">
-                    <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px] ">Платформа развлечений на борту ПАО «Аэрофлот»</p>
-                </div>
-                <div class="flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
-                    <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-ugmk.png') }}" alt="">
-                    <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">3D-тренажер для УГМК</p>
-                </div>
-                <div class="flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
-                    <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-uni.png') }}" alt="">
-                    <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">«Умная обувь»: прототип обуви будущего</p>
-                </div>
-                <div class="flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
-                    <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-mus.png') }}" alt="">
-                    <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">Виртуальный атлас-тренажер для ООО «ПрограмЛаб»</p>
-                </div>
-                <div class="flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
-                    <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-hyatt.png') }}" alt="">
-                    <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">Business case for human resources management</p>
-                </div>
-                <div class="flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
-                    <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-bank.png') }}" alt="">
-                    <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">Выявление актуальных схем финансового мошенничества</p>
-                </div>
+            <div class="swiper SwiperWelcomepartners w-[100%]">
+                <div class="swiper-wrapper flex flex-row grow gap-[8px] w-[100%]">
+                    <div class="swiper-slide flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
+                        <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-aero.png') }}" alt="">
+                        <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px] ">Платформа развлечений на борту ПАО «Аэрофлот»</p>
+                    </div>
+                    <div class="swiper-slide flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
+                        <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-ugmk.png') }}" alt="">
+                        <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">3D-тренажер для УГМК</p>
+                    </div>
+                    <div class="swiper-slide flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
+                        <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-uni.png') }}" alt="">
+                        <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">«Умная обувь»: прототип обуви будущего</p>
+                    </div>
+                    <div class="swiper-slide flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
+                        <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-mus.png') }}" alt="">
+                        <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">Виртуальный атлас-тренажер для ООО «ПрограмЛаб»</p>
+                    </div>
+                    <div class="swiper-slide flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
+                        <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-hyatt.png') }}" alt="">
+                        <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">Business case for human resources management</p>
+                    </div>
+                    <div class="swiper-slide flex flex-1 flex-col grow sm:gap-[20px] min-[320px]:gap-[12px]">
+                        <img class="min-w-[126px]" src="{{ Vite::asset('resources/images/part-bank.png') }}" alt="">
+                        <p class="text-white font-inter md:text-[15px] min-[320px]:text-[12px]">Выявление актуальных схем финансового мошенничества</p>
+                    </div>
+                </div> 
             </div>
         </section>
         <section class="flex lg:flex-row min-[320px]:flex-col gap-[48px] w-[100%] place-content-between">
@@ -108,6 +116,7 @@
                <x-cardtwo></x-cardtwo>
             </div>
         </section>
+        <x-form-order></ x-form-order>
         <section class="about-form grid grid-cols-2 font-inter w-[100%] font-inter">
             
         </section>
