e7ae3e78ca45f79f066c0993b0cff639775c2eb8
Sophie <bersenjowa@gmail.com>
2025-06-27 09:55:27 +0500

    add pop-up

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -179,5 +179,12 @@
 </section>
 
 <x-form-order></x-form-order>
+
+<!-- <div class="pop-up bg-white flex items-center justify-between rounded-3xl py-4 px-4 ">
+    <h3 class="text-black font-victor font-semibold  grow">Ваша заявка у нас! Спасибо, что записались — очень скоро мы свяжемся с вами.</h3>
+    <button class="x-close bg-black/15 flex rounded-[100px] border border-black/20 p-2 hover:bg-black/20 focus:outline-black">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#111111" viewBox="0 0 256 256"><path d="M202.83,197.17a4,4,0,0,1-5.66,5.66L128,133.66,58.83,202.83a4,4,0,0,1-5.66-5.66L122.34,128,53.17,58.83a4,4,0,0,1,5.66-5.66L128,122.34l69.17-69.17a4,4,0,1,1,5.66,5.66L133.66,128Z"></path></svg>
+    </button>
+</div> -->
 </div>
 </x-app-layout>
