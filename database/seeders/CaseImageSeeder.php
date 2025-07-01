b19837c932e5f6272f3453e346efcb0361a03b3e
JuliaMuk <Lector@rbiu.local>
2025-07-01 09:51:31 +0500

    add images to cases

--- database/seeders/CaseImageSeeder.php
+++ database/seeders/CaseImageSeeder.php
@@ -16,12 +16,12 @@ class CaseImageSeeder extends Seeder
         CaseImage::insert(
             [
                 [
-                    'path_img' => 'content/case1.png',
+                    'path_img' => 'content/behance/сhelbasket/redisign.jpg',
                     'work_id' => 1,
                     
                 ],
                 [
-                    'path_img' => 'content/case1.png',
+                    'path_img' =>  'content/behance/сhelbasket/stageswork.jpg',
                     'work_id' => 1,
                 ]
             ]
